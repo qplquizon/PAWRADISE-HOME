1ecec67239bb8e54a65e6315eca03eb3a819c832
GOROMAJIMA09 <hanzsicat00@gmail.com>
2025-10-02 18:27:17 +0800

    qwerty

--- admin_panel.php
+++ admin_panel.php
@@ -175,7 +175,7 @@ try {
         </div>
     </div>
 
-    <!-- Delete Confirmation Modal -->
+ 
     <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -198,6 +198,7 @@ try {
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="adopt.js"></script>
     <script>
         var deleteModal = document.getElementById('deleteModal');
         deleteModal.addEventListener('show.bs.modal', function (event) {
